196d89dea46287d6d0586feb73210142ccb65551
Winston Wright <winstonwright88@gmail.com>
2015-10-12 17:03:08 +0100

    Random Bag System Edit
    previous commit didn't get the last save in

--- Easy/tetris.py
+++ Easy/tetris.py
@@ -1,8 +1,11 @@
 # Random Bag System, 12 Oct 2015, https://www.reddit.com/r/dailyprogrammer/comments/3ofsyb/20151012_challenge_236_easy_random_bag_system/
 
+from random import *
+
 letters=['O','I','S','Z','L','J','T']
 result=""
 for x in range(7):
-	random.shuffle(letters)
+	shuffle(letters)
 	result+="".join(letters)
-result+=letters[random(7)]
+result+=letters[randint(0,6)]
+print (result)
